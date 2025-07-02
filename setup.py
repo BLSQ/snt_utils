e8a29313480a6c18837f2873fba672fb4f34e4f5
EMontandon <esteban14m@gmail.com>
2025-07-02 11:01:30 +0200

    setup fix

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="snt_pipeline_utils",
+    name="snt_lib",
     version="0.1",
     packages=find_packages(),  # Automatically finds snt_lib
 )
